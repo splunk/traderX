0f3c998f9d04de2a013220d86f1ef6f4026f82b8
rcastley <rcastley@splunk.com>
2024-09-11 12:44:06 +0100

    Simple loadgen

---
+++ loadgen/trades.py
@@ -0,0 +1,40 @@
+import requests
+import random
+import time
+
+while True:
+    account_ids = [
+    22214,
+    11413,
+    42422,
+    52355,
+    62654,
+    10031,
+    44044
+    ]
+
+    security = [
+    "MMM",
+    "AOS",
+    "ABT",
+    "ABBV",
+    "CPB",
+    "COF"
+    ]
+
+    side = [
+        "Buy",
+        "Sell"
+    ]
+
+    headers = {
+        'Content-Type': 'application/json',
+        'accept': 'application/json',
+    }
+
+    data = {"quantity":random.randint(1,1000),"accountId":random.choice(account_ids),"side":random.choice(side),"security":random.choice(security)}
+
+
+    response = requests.post('http://localhost:18092/trade/', headers=headers, json=data)
+    print(response.text)
+    time.sleep(1)
